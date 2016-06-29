e43b7fafb038ea2487604ddddd12213bf23efefe
Andy Erickson <andy.w.erickson@gmail.com>
2016-06-28 22:28:27 -0500

    add cool ascii faces

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 var app = require('express')();
+var cool = require('cool-ascii-faces');
 
 app.set('port', (process.env.PORT || 3000));
 
 app.get('/', function(req, res){
     res.send('Hello Heroku');
+});
+
+app.get('/cool', function(req, res){
+    res.send(cool());
 });
 
 app.listen(app.get('port'), function(){
