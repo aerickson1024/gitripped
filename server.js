e6cb85191331a42065c750e6486a9530d5ceeba4
Andy Erickson <andy.w.erickson@gmail.com>
2016-06-29 22:12:25 -0500

    made some minor modifications

--- server.js
+++ server.js
@@ -1,15 +1,9 @@
-var app = require('express')();
-var cool = require('cool-ascii-faces');
+var express = require('express'),
+    app = express();
+
+app.use(express.static(__dirname + '/public'));
 
 app.set('port', (process.env.PORT || 3000));
-
-app.get('/', function(req, res){
-    res.send('Hello Heroku');
-});
-
-app.get('/cool', function(req, res){
-    res.send(cool());
-});
 
 app.listen(app.get('port'), function(){
     console.log('server is now running on port %s', app.get('port'));
