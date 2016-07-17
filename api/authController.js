c9e922b0208d39014e5c2ae0255378d9b57f5303
Andy Erickson <andy.w.erickson@gmail.com>
2016-07-17 14:23:49 -0500

    changed around some stuff.  nothing added

--- api/authController.js
+++ api/authController.js
@@ -5,14 +5,14 @@ module.exports = function(app) {
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
 
-    app.post('/api/authenticate', function(req, res) {
+    app.post('/api/register', function(req, res) {
         res.send({
             email: req.body.email,
             password: req.body.password
         });
     });
 
-    app.post('/api/register', function(req, res) {
+    app.post('/api/authenticate', function(req, res) {
         res.send({
             email: req.body.email,
             password: req.body.password
--- public/app/landing/landing.controller.js
+++ public/app/landing/landing.controller.js
@@ -8,15 +8,15 @@
     function Landing(user) {
         var vm = this;
 
-        vm.login = function() {
-            user.authenticate(vm.email, vm.password).then(function(res) {
-                console.log('Authenticated: %s %s', res.data.email, res.data.password);
-            });
-        }
-
         vm.register = function() {
             user.register(vm.email, vm.password).then(function(res) {
                 console.log('Registered: %s %s', res.data.email, res.data.password);
+            });
+        }
+
+        vm.login = function() {
+            user.authenticate(vm.email, vm.password).then(function(res) {
+                console.log('Authenticated: %s %s', res.data.email, res.data.password);
             });
         }
     }
