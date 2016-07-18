ad291cdd57db1209ff9f3978da282c87190687ea
Andy Erickson <andy.w.erickson@gmail.com>
2016-07-18 18:40:22 -0500

    the login button is disabled if both fields are populated

--- client/app/login/login.controller.js
+++ client/app/login/login.controller.js
@@ -9,6 +9,7 @@
         var vm = this;
 
         vm.login = function() {
+            console.log('this was hit');
             user.authenticate(vm.email, vm.password).then(function(res) {
                 if (res.data.success) {
                     $location.path('/dashboard');
